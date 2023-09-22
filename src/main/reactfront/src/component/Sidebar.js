db4f3a7adf2669ca89b0f8b5cfbd3cc7ee5f97a5
yjy12681 <yjy454@naver.com>
2023-09-22 12:44:36 +0900

    yjy

--- src/main/reactfront/src/component/Sidebar.js
+++ src/main/reactfront/src/component/Sidebar.js
@@ -1,119 +1,139 @@
-import { Link } from "react-router-dom";
-import "../styles/Style.css";
+import { Link } from 'react-router-dom';
+import '../styles/Style.css';
+import { tokenInfoContext } from './TokenInfoProvider';
+import React, { useContext } from 'react';
 
 function Sidebar() {
-
-  if (window.location.pathname === "/") return null;
+  const { userRole, username, handleChange } = useContext(tokenInfoContext);
+  if (window.location.pathname === '/') return null;
 
   return (
     <aside id="sidebar" className="sidebar">
       <ul className="sidebar-nav" id="sidebar-nav">
-        <li className="nav-item">
-          <Link to="/user/userMain" className="nav-link">
-            <i className="bi bi-grid"></i>
-            <span>사원메인페이지</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/admin/adminMain" className="nav-link">
-            <i className="bi bi-grid"></i>
-            <span>관리자메인페이지</span>
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/highadmin/highAdminMain" className="nav-link">
-            <i className="bi bi-grid"></i>
-            <span>상위관리자메인페이지</span>
-          </Link>
-        </li>
-
-        <li className="nav-item">
-          <Link
-            to="/"
-            className="nav-link collapsed"
-            data-bs-target="#components-nav"
-            data-bs-toggle="collapse">
-            <i className="bi bi-menu-button-wide"></i>
-            <span>IT 자산 목록</span>
-            <i className="bi bi-chevron-down ms-auto"></i>
-          </Link>
-          <ul
-            id="components-nav"
-            className="nav-content collapse "
-            data-bs-parent="#sidebar-nav">
-            <li>
-              <Link to="/itassets">
-                <span>전체 목록</span>
-              </Link>
-            </li>
-            <li className="accordion-item" style={{ border: "none" }}>
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#flush-collapseOne"
-                aria-expanded="false"
-                aria-controls="flush-collapseOne">
-                <span className="nav-category-sidebar">카테고리별</span>
-              </button>
-              <div
-                id="flush-collapseOne"
-                className="accordion-collapse collapse"
-                aria-labelledby="flush-headingOne"
-                data-bs-parent="#accordionFlushExample">
-                <Link to="/itassets/pc">
-                  <div
-                    className="accordion-body nav-category-sidebar"
-                    style={{ marginTop: "-15px" }}>
-                    PC/노트북
-                  </div>
-                </Link>
-              </div>
-              <div
-                id="flush-collapseOne"
-                className="accordion-collapse collapse"
-                aria-labelledby="flush-headingOne"
-                data-bs-parent="#accordionFlushExample">
-                <Link to="/itassets/sw">
-                  <div
-                    className="accordion-body nav-category-sidebar"
-                    style={{ marginTop: "-25px" }}>
-                    소프트웨어
-                  </div>
-                </Link>
-              </div>
-              <div
-                id="flush-collapseOne"
-                className="accordion-collapse collapse"
-                aria-labelledby="flush-headingOne"
-                data-bs-parent="#accordionFlushExample">
-                <Link to="/itassets/sv">
-                  <div
-                    className="accordion-body nav-category-sidebar"
-                    style={{ marginTop: "-25px" }}>
-                    서버
-                  </div>
-                </Link>
-              </div>
-              <div
-                id="flush-collapseOne"
-                className="accordion-collapse collapse"
-                aria-labelledby="flush-headingOne"
-                data-bs-parent="#accordionFlushExample">
-                <Link to="/itassets/etc">
-                  <div
-                    className="accordion-body nav-category-sidebar"
-                    style={{ marginTop: "-25px" }}>
-                    기타
-                  </div>
-                </Link>
-              </div>
-            </li>
-            <Link to="/itassets/use">
-              <span>자산 사용 일괄 신청</span>
+        {userRole == 'ROLE_USER' && (
+          <li className="nav-item">
+            <Link to="/user/userMain" className="nav-link">
+              <i className="bi bi-grid"></i>
+              <span>사원메인페이지</span>
             </Link>
-          </ul>
-        </li>
+          </li>
+        )}
+        {userRole == 'ROLE_ADMIN' && (
+          <li className="nav-item">
+            <Link to="/admin/adminMain" className="nav-link">
+              <i className="bi bi-grid"></i>
+              <span>관리자메인페이지</span>
+            </Link>
+          </li>
+        )}
+        {userRole == 'ROLE_HIGH_ADMIN' && (
+          <li className="nav-item">
+            <Link to="/highadmin/highAdminMain" className="nav-link">
+              <i className="bi bi-grid"></i>
+              <span>상위관리자메인페이지</span>
+            </Link>
+          </li>
+        )}
+        {userRole == 'ROLE_USER' && (
+          <li className="nav-item">
+            <Link
+              to="/"
+              className="nav-link collapsed"
+              data-bs-target="#components-nav"
+              data-bs-toggle="collapse"
+            >
+              <i className="bi bi-menu-button-wide"></i>
+              <span>IT 자산 목록</span>
+              <i className="bi bi-chevron-down ms-auto"></i>
+            </Link>
+            <ul
+              id="components-nav"
+              className="nav-content collapse "
+              data-bs-parent="#sidebar-nav"
+            >
+              <li>
+                <Link to="/itassets">
+                  <span>전체 목록</span>
+                </Link>
+              </li>
+              <li className="accordion-item" style={{ border: 'none' }}>
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseOne"
+                >
+                  <span className="nav-category-sidebar">카테고리별</span>
+                </button>
+                <div
+                  id="flush-collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingOne"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <Link to="/itassets/pc">
+                    <div
+                      className="accordion-body nav-category-sidebar"
+                      style={{ marginTop: '-15px' }}
+                    >
+                      PC/노트북
+                    </div>
+                  </Link>
+                </div>
+                <div
+                  id="flush-collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingOne"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <Link to="/itassets/sw">
+                    <div
+                      className="accordion-body nav-category-sidebar"
+                      style={{ marginTop: '-25px' }}
+                    >
+                      소프트웨어
+                    </div>
+                  </Link>
+                </div>
+                <div
+                  id="flush-collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingOne"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <Link to="/itassets/sv">
+                    <div
+                      className="accordion-body nav-category-sidebar"
+                      style={{ marginTop: '-25px' }}
+                    >
+                      서버
+                    </div>
+                  </Link>
+                </div>
+                <div
+                  id="flush-collapseOne"
+                  className="accordion-collapse collapse"
+                  aria-labelledby="flush-headingOne"
+                  data-bs-parent="#accordionFlushExample"
+                >
+                  <Link to="/itassets/etc">
+                    <div
+                      className="accordion-body nav-category-sidebar"
+                      style={{ marginTop: '-25px' }}
+                    >
+                      기타
+                    </div>
+                  </Link>
+                </div>
+              </li>
+              <Link to="/itassets/use">
+                <span>자산 사용 일괄 신청</span>
+              </Link>
+            </ul>
+          </li>
+        )}
         {/* <!-- End Components Nav --> */}
 
         <li className="nav-item">
@@ -121,25 +141,21 @@ function Sidebar() {
             to="####"
             className="nav-link collapsed"
             data-bs-target="#forms-nav"
-            data-bs-toggle="collapse">
+            data-bs-toggle="collapse"
+          >
             <i className="bi bi-journal-text"></i>
-            <span>나의 신청목록</span>
+            <span>IT 자산</span>
             <i className="bi bi-chevron-down ms-auto"></i>
           </Link>
           <ul
             id="forms-nav"
             className="nav-content collapse "
-            data-bs-parent="#sidebar-nav">
+            data-bs-parent="#sidebar-nav"
+          >
             <li>
-              <Link to="/####">
+              <Link to="/adminitassets">
                 <i className="bi bi-circle"></i>
-                <span>Form Eleme</span>
-              </Link>
-            </li>
-            <li>
-              <Link to="####">
-                <i className="bi bi-circle"></i>
-                <span>Form Layouts</span>
+                <span>재고 관리</span>
               </Link>
             </li>
           </ul>
@@ -151,7 +167,8 @@ function Sidebar() {
             to="####"
             className="nav-link collapsed"
             data-bs-target="#tables-nav"
-            data-bs-toggle="collapse">
+            data-bs-toggle="collapse"
+          >
             <i className="bi bi-layout-text-window-reverse"></i>
             <span>결제 신청</span>
             <i className="bi bi-chevron-down ms-auto"></i>
@@ -159,7 +176,8 @@ function Sidebar() {
           <ul
             id="tables-nav"
             className="nav-content collapse "
-            data-bs-parent="#sidebar-nav">
+            data-bs-parent="#sidebar-nav"
+          >
             <li>
               <Link to="####">
                 <i className="bi bi-circle"></i>
@@ -178,18 +196,20 @@ function Sidebar() {
 
         <li className="nav-item">
           <Link
-              to="#"
-              className="nav-link collapsed"
-              data-bs-target="#charts-nav"
-              data-bs-toggle="collapse">
+            to="#"
+            className="nav-link collapsed"
+            data-bs-target="#charts-nav"
+            data-bs-toggle="collapse"
+          >
             <i className="bi bi-bar-chart"></i>
             <span>사용자 신청</span>
             <i className="bi bi-chevron-down ms-auto"></i>
           </Link>
           <ul
-              id="charts-nav"
-              className="nav-content collapse "
-              data-bs-parent="#sidebar-nav">
+            id="charts-nav"
+            className="nav-content collapse "
+            data-bs-parent="#sidebar-nav"
+          >
             <li>
               <Link to="/admin/approveList">
                 <i className="bi bi-circle"></i>
@@ -218,18 +238,20 @@ function Sidebar() {
         </li>
         <li className="nav-item">
           <Link
-              to="#"
-              className="nav-link collapsed"
-              data-bs-target="#charts-nav"
-              data-bs-toggle="collapse">
+            to="#"
+            className="nav-link collapsed"
+            data-bs-target="#high-nav"
+            data-bs-toggle="collapse"
+          >
             <i className="bi bi-bar-chart"></i>
             <span>관리자 신청</span>
             <i className="bi bi-chevron-down ms-auto"></i>
           </Link>
           <ul
-              id="charts-nav"
-              className="nav-content collapse "
-              data-bs-parent="#sidebar-nav">
+            id="high-nav"
+            className="nav-content collapse "
+            data-bs-parent="#sidebar-nav"
+          >
             <li>
               <Link to="/highAdmin/approveList">
                 <i className="bi bi-circle"></i>
@@ -254,10 +276,14 @@ function Sidebar() {
                 <span>최종 구매 처리 목록</span>
               </Link>
             </li>
-
+            <li>
+              <Link to="/itassetsapproval">
+                <i className="bi bi-circle"></i>
+                <span>폐기/수리 처리 목록</span>
+              </Link>
+            </li>
           </ul>
         </li>
-
 
         {/* <!-- End Charts Nav --> */}
 
@@ -266,7 +292,8 @@ function Sidebar() {
             to="####"
             className="nav-link collapsed"
             data-bs-target="#icons-nav"
-            data-bs-toggle="collapse">
+            data-bs-toggle="collapse"
+          >
             <i className="bi bi-gem"></i>
             <span>사용자 관리</span>
             <i className="bi bi-chevron-down ms-auto"></i>
@@ -274,7 +301,8 @@ function Sidebar() {
           <ul
             id="icons-nav"
             className="nav-content collapse "
-            data-bs-parent="#sidebar-nav">
+            data-bs-parent="#sidebar-nav"
+          >
             <li>
               <Link to="users/userReg">
                 <i className="bi bi-circle"></i>
